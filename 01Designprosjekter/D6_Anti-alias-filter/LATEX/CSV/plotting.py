f0cad415931f5c81bf82439f1e40adac892189b2
Peter Pham <phpham@stud.ntnu.no>
2022-10-03 00:29:53 +0200

    snart?

--- 01Designprosjekter/D6_Anti-alias-filter/LATEX/CSV/plotting.py
+++ 01Designprosjekter/D6_Anti-alias-filter/LATEX/CSV/plotting.py
@@ -1,13 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import os
-
 
 header = []
 data = []
-dir_containing_file = 'C:/Users/peter/Documents/ESDA2/01Designprosjekter/D6_Anti-alias-filter/LATEX/CSV'
-filename = 'vout.csv'
+filename = 'C://Users//peter//Documents//ESDA2//01Designprosjekter//D6_Anti-alias-filter//LATEX//CSV//vout2.csv'
 
 #Henter data fra csvfil
 with open(filename) as csvfile:
@@ -20,15 +17,20 @@ with open(filename) as csvfile:
 #Legger inn data fra hver kanal i hver sin liste
 time = [(p[0]) for p in data]
 ch1 = [(p[1]) for p in data]
-ch2 = [(p[2]) for p in data]
+# ch2 = [(p[2]) for p in data]
 plt.plot(time, ch1)
 # plt.plot(time, ch2)
 fig, (ax) = plt.subplots(1,1)
 ax.plot(time,ch1, label = 'vout')
 # ax.plot(time,ch2, label = 'ch2')
+plt.plot(2400, -3.58278, 'ro')
+plt.plot(3200, -10.32255, 'ro')
+ax.annotate('(2400, -3.58278)', [2000, -1.93887])
+ax.annotate('(3200, -10.32255)', [2800, -8.35529])
 ax.grid(True)
-ax.set_title('Oscilloskop')
-ax.set_xlabel('Tid (s)')
-ax.set_ylabel('Spenning (V))')
+# ax.set_title('Oscilloskop')
+ax.set_xlabel('Frekvens (Hz)')
+ax.set_ylabel('Demping (dB)')
 ax.set_xscale('log')
+ax.legend(loc='upper right')
 plt.show()
